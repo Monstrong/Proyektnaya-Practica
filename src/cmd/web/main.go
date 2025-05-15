c59831c097d1e59a83ccd4401e6753fca60e7cae
Monstrong <igrachev2006@gmail.com>
2025-05-15 20:12:19 +0300

    добавлено кэширование шаблонов
    также шаблоны разделены на общие layput шаблон с хэдером и футером, и сами шаблоны с контентом страниц

--- src/cmd/web/main.go
+++ src/cmd/web/main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/monstrong/proyektnaya-practica/src/pkg/config"
 	"github.com/monstrong/proyektnaya-practica/src/pkg/handlers"
+	"github.com/monstrong/proyektnaya-practica/src/pkg/render"
 )
 
 var portNumber = ":8080"
 
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache(); if err != nil {
+		log.Fatal("cannot create template cache, ", err)
+	}
+	app.TemplateCache = tc
+
+	render.NewTemplates(&app)
+
 	// Обработчик статических файлов (CSS, JS, изображения)
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
--- src/pkg/render/render.go
+++ src/pkg/render/render.go
@@ -1,15 +1,79 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
+
+	"github.com/monstrong/proyektnaya-practica/src/pkg/config"
 )
 
+var functions = template.FuncMap{
+
+}
+
+var app *config.AppConfig
+
+//sets the config for the template packege
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./site_templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
+	// get the template cache from the app config
+
+	tc := app.TemplateCache
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("Could not get template from template cache")
 	}
+
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, nil)
+
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing template to browser: ", err)
+	}
+}
+
+// создаем template cache как map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./site_templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		fmt.Println("Page is currently", page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./site_templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./site_templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+
+		}
+
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
